fb5bef49f21ccf9805669691e9242ec9989b4a16
unknown <abdullahazzam79310@gmail.com>
2023-11-04 22:37:07 +0700

    update

--- pekan6/tugas6.2.py
+++ pekan6/tugas6.2.py
@@ -4,3 +4,13 @@ for i in range(1, 20, 2):
     total += i
 
 print(f"1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 = {total}")
+# cara yg lain
+bilangan = 1
+jumlah =0
+
+while bilangan <= 19:
+    print (bilangan, end=" + ")
+    jumlah += bilangan
+    bilangan += 2
+
+print("\b\b=",jumlah)
